26fd4703d257d95ee68af5bbb309f7b7ac39219f
chickennuggetduck33 <luke.fun59@gmail.com>
2023-11-11 16:25:47 -0800

    Finish the find the hidden button code

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -16,12 +16,14 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class _02_FindTheHiddenButton implements ActionListener{
+	Random rand = new Random();
 	JFrame window;
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] buttons;
 	//2 create an int variable called hiddenButton
+	int hiddenButton;
 	
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
@@ -31,28 +33,35 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window = new JFrame("Find the Button");
 		panel = new JPanel();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		String posInp = JOptionPane.showInputDialog("Hello user, Please input a positive number: ");
+		int posInt = Integer.parseInt(posInp);
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+		buttons = new JButton[posInt];
 		//5. Make a for loop to iterate through the JButton array
+			for (int i = 0; i < buttons.length; i++) {
 			//6. initialize each JButton in the array
+				JButton button = new JButton();
 			//7. add the ActionListener to each JButton
+				button.addActionListener(this);
+				buttons[i] = button;
 			//8. add each JButton to the panel
-		
-		//9 add the panel to the window
-		
-		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
-		//11. set the JFrame to visible.
-		
-		//12. Give the user the instructions for the game.
-		
-		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
-		
-		//14. Set the text of the JButton located at hiddenButton to  "ME"
+				panel.add(button);
 
+			}
+		//9 add the panel to the window
+		window.add(panel);
+		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		//11. set the JFrame to visible.
+		window.setVisible(true);
+		//12. Give the user the instructions for the game.
+		JOptionPane.showMessageDialog(null, "Find the hidden number");
+		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
+		int random = rand.nextInt(1000);
+		hiddenButton = random;
+		//14. Set the text of the JButton located at hiddenButton to  "ME"
+		buttons[hiddenButton].setText("Me"d);
 		//15. Use Thread.sleep(1000); to pause the program.
 		//    Surround it with a try/catch - use Eclipse helper for this
 		
